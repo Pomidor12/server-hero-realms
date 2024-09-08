7a5ca29997328d23a2a7083832a43e61ee9d2e10
Bezlkiy <seki4calm@gmail.com>
2024-09-08 21:04:11 +0500

    🗃️ db: изменены уникальные поля для таблиц

--- prisma/migrations/20240908155819_change_fields_to_unique_and_optional/migration.sql
+++ prisma/migrations/20240908155819_change_fields_to_unique_and_optional/migration.sql
@@ -1,3 +1,6 @@
+-- DropIndex
+DROP INDEX "hero_name_key";
+
 -- AlterTable
 ALTER TABLE "action" DROP CONSTRAINT "action_pkey",
 ADD COLUMN     "id" SERIAL NOT NULL,
@@ -13,6 +16,12 @@ ALTER COLUMN "round" SET DEFAULT 1;
 ALTER TABLE "hero" ALTER COLUMN "protection" DROP NOT NULL,
 ALTER COLUMN "isGuardian" SET DEFAULT false,
 ALTER COLUMN "placement" DROP NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "battlefield_name_key" ON "battlefield"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "hero_unique_keys" ON "hero"("id", "name", "player_id", "battlefield_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "player_name_key" ON "player"("name");
